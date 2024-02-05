99c196c7aa836e16a5e7d1923913c3c9cba0f200
DhineshKumar5089 <dineshnivetha2004@gmail.com>
2024-02-05 08:08:01 +0530

    commit

--- set.py
+++ set.py
@@ -2,7 +2,20 @@ c={"Sql","Wd","Python"}
 d={"Sql","Wd","Java"}
 e=c&d
 print(e)
+e=c|d
+print(e)
+e=c-d
+print(e)
+e=c^d
+print(e)
 
-
+#for clearing a duplicate value in a list.
 a=[1,2,1,5,2,8]
 print(set(a))
+
+a=[1,2,1,5,2,8]
+b=[]
+for i in a:
+    if i not in b:
+        b.append(i)
+print(b)        
